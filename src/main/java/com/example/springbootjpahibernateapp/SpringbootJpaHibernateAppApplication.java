153e2af2ef1d3764b1ed090af7daee46a35e242d
Alexandr <alexandrsinko2000@gmail.com>
2022-05-08 15:53:24 +0300

    initial commit

---
+++ src/main/java/com/example/springbootjpahibernateapp/SpringbootJpaHibernateAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpahibernateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaHibernateAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaHibernateAppApplication.class, args);
+    }
+
+}
